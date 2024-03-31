bd1cbd6dda0776c431965ccd386b79c41a90c21e
tais defante <taisadefante@hotmail.com>
2024-03-30 21:12:42 -0300

    alteração projeto cadastro

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-var acc = document.getElementsByClassName('accordion');
+var acc = document.getElementsByClassName("accordion");
 var i;
 
-for(i = 0; i < acc.length; i++) {
-    acc[i].addEventListener('click', function(){
-        this.classList.toggle('active');
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
 
-        var panel = this.nextElementSibling;
-        if(panel.style.display === 'block') {
-            panel.style.display = 'none';
-        } else {
-            panel.style.display = 'block';
-        }
-    })
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function cadastrar() {
+  const nome = document.getElementById("nome").value;
+  const tel = document.getElementById("telefone").value;
+  const email = document.getElementById("email").value;
+  const end = document.getElementById("endereco").value;
+  const serv = document.getElementById("servico").value;
+
+  alert(`Nome: ${nome}\nTelefone: ${tel}\nEmail: ${email}\nEndereço: ${end}`);
 }
